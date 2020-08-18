dc6c2af342425cb1cb5901f583ca7d33b5b9056b
Siebe Dreesen <siebe.dreesen@student.uhasselt.be>
2020-08-18 16:31:23 +0200

    Column fixed

--- app/controllers/jobs/show.js
+++ app/controllers/jobs/show.js
@@ -3,7 +3,6 @@ import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 
 export default class JobsShowController extends Controller {
-    @tracked columnName;
     @tracked selectedColumn;
 
 }
---
+++ tests/integration/helpers/json-parser-test.js
@@ -0,0 +1,17 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Helper | json-parser', function(hooks) {
+  setupRenderingTest(hooks);
+
+  // TODO: Replace this with your real tests.
+  test('it renders', async function(assert) {
+    this.set('inputValue', '1234');
+
+    await render(hbs`{{json-parser inputValue}}`);
+
+    assert.equal(this.element.textContent.trim(), '1234');
+  });
+});
