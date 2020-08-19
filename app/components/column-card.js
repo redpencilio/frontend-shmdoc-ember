ae6bb8b31cf65b3bec4566200fb76e3454031e37
Siebe Dreesen <siebe.dreesen@student.uhasselt.be>
2020-08-19 16:31:26 +0200

    Unit bug fixed

--- app/components/column-card.js
+++ app/components/column-card.js
@@ -13,7 +13,7 @@ export default class ColumnCardComponent extends Component {
     @tracked columnNote;
     @tracked columnDataType;
     @tracked columnQuantityKind;
-    @tracked columnUnitName;
+    @tracked columnUnit;
     @tracked columnDisableProcessing;
 
     @action editColumn(column) {
@@ -22,7 +22,7 @@ export default class ColumnCardComponent extends Component {
         this.columnNote = this.selectedColumn.note;
         this.columnDataType = this.selectedColumn.dataType;
         this.columnQuantityKind = this.selectedColumn.quantityKind;
-        this.columnUnitName = this.selectedColumn.unit.name;
+        this.columnUnit = this.selectedColumn.unit;
         this.columnDisableProcessing = this.selectedColumn.disableProcessing;
         this.toggleEditColumn();
     }
@@ -42,12 +42,12 @@ export default class ColumnCardComponent extends Component {
     }
 
     @action async confirmEdit() {
-        const column = await this.store.findRecord('column', this.selectedColumn.id);        
+        const column = await this.store.findRecord('column', this.selectedColumn.id);       
         column.description = this.columnDescription;
         column.note = this.columnNote;
         column.dataType = this.columnDatatype;
         column.quantityKind = this.columnQuantityKind;
-        column.unit.name = this.columnUnitName;
+        column.unit = this.columnUnit;
         column.disableProcessing = this.columnDisableProcessing;
         column.save();
         this.toggleEditColumn();
--- app/routes/jobs/show.js
+++ app/routes/jobs/show.js
@@ -1,11 +1,14 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
-
+import RSVP from 'rsvp';
 
 export default class JobsShowRoute extends Route {
     @service store;
     
     async model(params) {
-        return this.store.find('schema-analysis-job', params["schema-analysis-job_id"]);
+        return RSVP.hash({
+            job: this.store.find('schema-analysis-job', params["schema-analysis-job_id"]),
+            units: this.store.findAll('unit')
+          });
     }
 }
