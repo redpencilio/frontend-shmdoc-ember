4891e38b0e37e14624b3f69b150fa9b2aac5e6cb
Siebe Dreesen <siebe.dreesen@student.uhasselt.be>
2020-09-02 16:02:43 +0200

    Auto refresh on add new analysis

--- app/controllers/sources/show.js
+++ app/controllers/sources/show.js
@@ -1,6 +1,7 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
+import { getOwner } from '@ember/application';
 
 export default class SourcesShowController extends Controller { 
     @tracked addingAnalysis = false;
@@ -9,6 +10,10 @@ export default class SourcesShowController extends Controller {
     @tracked sourceName = this.get('model').name;
     @tracked sourceDescription = this.get('model').description;
     @tracked sourceNote = this.get('model').note;
+
+    @tracked files = [];
+    @tracked filesEmpty = true;
+    @tracked selectedAnalysis = null;
 
     @action showJob(job) {
         this.transitionToRoute("sources.show.jobs.show", job.id)
@@ -36,5 +41,45 @@ export default class SourcesShowController extends Controller {
         source.note = this.sourceNote;
         source.save();
         this.toggleEditSource();
+    }
+
+    @action 
+    handleFileChange(file) {
+        this.files.pushObject(file);
+        this.filesEmpty = false;
+          
+      }
+
+    @action 
+    removeFile(file) {
+        this.files = this.files.filter(f => f.name !== file.name);
+        if (this.files.length == 0) {
+            this.filesEmpty = true;
+        }
+    }
+
+    @action 
+    async uploadFiles(sourceId) {
+        let d = new Date() 
+        let timestamp = d.toISOString()
+        let source = this.store.peekRecord( 'source', sourceId);
+        for (const file of this.files) {
+            let result = await file.upload("/files");
+            this.store.pushPayload('file', result.body);
+            let uploadedFile = this.store.peekRecord( 'file', result.body.data.id );
+
+            // If an analysis is selected, the new record will take the notes, description and unit from the selected analysis.
+            let resultJob = await this.store.createRecord('schema-analysis-job', {
+                source: source,
+                file: uploadedFile,
+                created: timestamp}).save();
+            await fetch(`/schema-analysis-jobs/${resultJob.id}/run`, {method:"POST"});
+            //this.store.findRecord("schemaAnalysisJob", resultJob.id, {reload: true});
+        }
+        source.save();
+        this.files = [];
+        this.filesEmpty = true;
+        // Refresh the dataTable
+        var controller = getOwner(this).lookup("route:sources/show/index").refresh();
     }
 }
